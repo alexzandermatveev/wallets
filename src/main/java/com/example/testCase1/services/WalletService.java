17129f36824ddd92716480bc6ea21962faa986b6
alexzandermatveev <alexzander20011@gmail.com>
2025-03-29 16:14:48 +0300

    queue deleted

--- src/main/java/com/example/testCase1/services/WalletService.java
+++ src/main/java/com/example/testCase1/services/WalletService.java
@@ -6,35 +6,23 @@ import com.example.testCase1.enums.OperationType;
 import com.example.testCase1.exceptions.InsufficientFundsException;
 import com.example.testCase1.exceptions.WalletNotFoundException;
 import com.example.testCase1.repository.WalletRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class WalletService {
     private final WalletRepository walletRepository;
 
-    // Очередь для операций
-    private final BlockingQueue<OperationRequest> operationQueue = new LinkedBlockingQueue<>();
-
-
-    // Асинхронная коллекция для синхронизации операций по кошельку
     private final ConcurrentHashMap<String, Object> walletLockMap = new ConcurrentHashMap<>();
 
-    // Основной метод, который будет добавлять операции в коллекцию и запускать обработку
     public Mono<Void> performOperation(OperationRequest request) {
         String walletId = request.getWalletId().toString();
 
-        // Проверяем, есть ли кошелек в процессе обработки
         Object lock = walletLockMap.computeIfAbsent(walletId, k -> new Object());  // Если нет, добавляем новый lock объект
 
         synchronized (lock) {
@@ -59,6 +47,7 @@ public class WalletService {
         return walletRepository.findById(walletId)
                 .switchIfEmpty(Mono.error(new WalletNotFoundException("Кошелек не найден")))
                 .map(Wallet::getBalance);
+
     }
 
 }
